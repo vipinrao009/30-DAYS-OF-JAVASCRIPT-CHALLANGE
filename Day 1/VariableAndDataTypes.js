602468247d780e743ab8379b0dd202a009eb4971
vipinrao009 <vipinrao2018@gmail.com>
2024-07-13 14:12:58 +0530

    Day 1

---
+++ Day 1/VariableAndDataTypes.js
@@ -0,0 +1,44 @@
+//Task 1
+
+var num = 2;
+console.log("TASK 1 : ",num);
+
+//Task 2
+
+let string = "Vipin Kumar"
+console.log("Task 2 : ",string);
+
+//Task 3
+
+const bool = true
+console.log("Task 3 : ",bool);
+
+//Task 4
+
+var num;
+let str= "";
+const bool1 = true;
+const object = {};
+const array = [];
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool1));
+console.log(typeof(object));
+console.log(typeof(array));
+
+//Task 5
+
+let num2 = 3;
+console.log("Initial value : ",num2);
+num2 = 4;
+console.log("New value : ",num2);
+
+
+//Task 6
+
+const vip = 4;
+console.log("1st value : ", vip);
+
+//vip = 6; // TypeError=> expolore the errore 
+//console.log("2nd value : ", vip);
